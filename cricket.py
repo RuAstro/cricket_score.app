a38763729018c9ea8596cb5a22e4cd0bccdd2bf4
Ruan vHeerden <vanheerdenruan43@gmail.com>
2024-05-31 11:16:40 +0200

    fixed spelling

--- cricket.py
+++ cricket.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """Cricket scoring app, to track score of the game. Makes scoring for coaches alot easier and faster, so that there is not a 
-            moment to miss on the field. Everything by just a click of buttons. The feuture of app will be updated regeurly. The 
-            idee of the app is to spare some time and have bit more outlook on whats happening on the field. And dont feel rush 
+    return """Cricket scoring app, to track score of the game. Makes scoring for coaches a lot easier and faster, so that there is not a 
+            moment to miss on the field. Everything by just a click of buttons. The feature of app will be updated regally. The 
+            idea of the app is to spare some time and have bit more outlook on whats happening on the field. And dont feel rush 
             for next tracking ball that follows. User can log in with same username and password, when register."""
 
 
