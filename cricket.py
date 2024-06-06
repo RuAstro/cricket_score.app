88b2c71fb631da4ecc58d6e7ba1dc43a9c1373e6
Ruan vHeerden <vanheerdenruan43@gmail.com>
2024-06-06 13:18:53 +0200

    added strike rate bowler and batsmen, and put it on the front page, so that it does not go through to much pages

--- cricket.py
+++ cricket.py
@@ -3,20 +3,38 @@ import logging
 
 app = Flask(__name__)
 
-logging.basicConfig(
-    filename="app.log",
-    level=logging.DEBUG,
-    format="%(asctime)s %(levelname)s %(message)s",
-)
+# logging.basicConfig(
+#     filename="app.log",
+#     level=logging.DEBUG,
+#     format="%(asctime)s %(levelname)s %(message)s",
+# )
 
 
 @app.route("/")
 def home():
     return """
-    <h1>Cricket Score calculation</h1>
-    <p>Click the button below to calculate the rates:</p>
+    <h1>Cricket Score Calculation</h1>
+    <p>Click the button below to calculate the run rate:</p>
     <form method="get" action="/calculate">
         <button type="submit">Calculate Run Rate</button>
+    </form>
+    
+    <br>
+    
+    <h2>Cricket Batsmen Strike Rate Calculation</h2>
+    <form method="post" action="/calculate_batsmen_strike_rate">
+        Runs: <input type="text" name="runs"><br>
+        Balls Faced: <input type="text" name="balls_faced"><br>
+        <button type="submit">Calculate Batsmen Strike Rate</button>
+    </form>
+    
+    <br>
+    
+    <h2>Cricket Bowler Strike Rate Calculation</h2>
+    <form method="post" action="/calculate_bowler_strike_rate">
+        Runs: <input type="text" name="runs"><br>
+        Balls Bowled: <input type="text" name="balls_bowled"><br>
+        <button type="submit">Calculate Bowler Strike Rate</button>
     </form>
     """
 
@@ -31,11 +49,26 @@ def calculate():
         run_rate, required_run_rate = calculate_run_rate(
             runs, overs, target_runs, remaining_overs
         )
-        app.logger.info("Calculation successful")
         return render_template(
             "result.html", run_rate=run_rate, required_run_rate=required_run_rate
         )
     return render_template("calculate.html")
+
+
+@app.route("/calculate_batsmen_strike_rate", methods=["POST"])
+def calculate_batsmen_strike_rate():
+    runs = int(request.form["runs"])
+    balls_faced = int(request.form["balls_faced"])
+    batsmen_strike_rate = (runs / balls_faced) * 100
+    return f"<h2>Strike Rate: {batsmen_strike_rate:.2f}</h2>"
+
+
+@app.route("/calculate_bowler_strike_rate", methods=["POST"])
+def calculate_bowler_strike_rate():
+    runs = int(request.form["runs"])
+    balls_bowled = int(request.form["balls_bowled"])
+    bowler_strike_rate = (runs / balls_bowled) * 100
+    return f"<h2>Strike Rate: {bowler_strike_rate:.2f}</h2>"
 
 
 def calculate_run_rate(runs, overs, target_runs, remaining_overs):
